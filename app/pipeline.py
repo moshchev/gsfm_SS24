5119fbd578b7b7ea52cb5285dd3363a6d4b9fd69
Marius <marius.gnoth@fs-students.de>
2024-02-27 23:28:03 +0100

    add stuff to pipeline prototyp

--- app/pipeline.py
+++ app/pipeline.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-
+#### LOAD DATASETS ####
 
 def load_datasets():
     """
@@ -20,7 +20,9 @@ def load_datasets():
     return df_forecasts, df_company
 
 
-def preprocessing(df):
+#### PREPROCESSING FUNCTIONS ####
+
+def preprocessing_ibes(df):
     """
     Preprocesses the input DataFrame by performing the following steps:
     1. Lowercase column names.
@@ -42,6 +44,10 @@ def preprocessing(df):
     # drop columns
     columns_to_drop = ["fpi","measure","cusip"]
     df_forecasts = df_forecasts.drop(columns=columns_to_drop)
+    
+    df_forecasts = df_forecasts.dropna(subset=['actual_eps'])
+    df_forecasts = df_forecasts.dropna(subset=['estimated_eps'])
+    
     
     def convert_to_datetime(df):
         columns_to_convert = ['fpedats','revdats', "anndats", 'anndats_act']
@@ -66,8 +72,54 @@ def preprocessing(df):
                                   "difference_date": "forecast_horizon"})
         return df_forecasts
     
-    df_forecasts = df.rename_columns(df_forecasts)
+    df_forecasts = rename_columns(df_forecasts)
     
     return df_forecasts
+
+
+def preprocessing_compustat(df):
+    # reduce to highest level of SIC code
+    df.loc[df['sic'].isna(), 'sic'] = -1 
+    df['sic'] = df['sic'].astype(str).str[:2]
+    df['sic'] = df['sic'].astype('int')
+    
+    
+#### ADD DV AND FEATURE FUNCTIONS ####
+
+def calculate_pmafe(df):
+    """
+    This function first calculates the absolute forecast error for each analyst i forecast of firm j in year t
+    If analyst i has multiple forecasts for firm j in year t, the function calculates the average forecast error
+    In the second step an extra column is calculated for the overall forecast error is calculate for each firm j in year t
+    In the third step, the function calculates the PMAFE for each analyst i forecast of firm j in year t
+    This function adds new columns to the dataframe:
+    - afe_analyst_i: the absolute forecast error for each analyst i forecast of firm j in year t
+    - afe_analyst_i_avg: the average absolute forecast error for each analyst i forecast of firm j in year t
+    - afe_mean_firm_j: the overall forecast error for each firm j in year t
+    - pmafe: the PMAFE for each analyst i forecast of firm j in year t
+    """
+    # Step 1: Calculate the average absolute forecast error for each analyst i forecast of firm j in year t
+    df['afe_analyst_i'] = np.abs(df['estimated_eps'] - df['actual_eps'])
+    
+    df_grouped = df.groupby(['ibes_ticker_pk', 'analyst', 'fiscal_period_ending']).agg({'afe_analyst_i': 'mean'}).reset_index()
+    df_grouped = df_grouped.rename(columns={'afe_analyst_i': 'afe_analyst_i_avg'})
+    df = pd.merge(df, df_grouped, on=['ibes_ticker_pk', 'analyst', 'fiscal_period_ending'], how='left')
+    
+    # Step 2: Calculate the overall forecast error for each firm j in year t
+    df['afe_mean_firm_j'] = df.groupby(["ibes_ticker_pk", "fiscal_period_ending"])["afe_analyst_i"].transform("mean")
+    
+    # Step 3: Calculate the PMAFE for each analyst i forecast of firm j in year t
+    df['pmafe'] = (df['afe_analyst_i_avg'] - df['afe_mean_firm_j']) / df['afe_mean_firm_j']
+    
+    return df
+
+
+
+
+
+
+
+
+#### COLLAPSE DATA ####
 
 
